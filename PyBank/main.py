911147a91a0d7c1219a3bad56420eb94f74ed664
eacromwell <ea.cromwell@gmail.com>
2023-03-13 18:43:26 -0600

    3.13 Final Commit
    3.13 Final Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,48 @@
 import csv
 
-# Read the CSV file and store the data in lists
-with open('Resources/budget_data.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader) # Skip the header row
+# Using with open to open the budget data csv
+with open('Resources/budget_data.csv', 'r') as budget_data_csv:
+    read_budget_data_csv = csv.reader(budget_data_csv, delimiter=',')
+    
+    # Stores the header row
+    header = next(budget_data_csv) 
+    
+    # Make dates and profits empty lists that we will add to
     dates = []
     profits = []
-    for row in csvreader:
+    
+    # Loop each row 
+    for row in read_budget_data_csv:
         dates.append(row[0])
         profits.append(int(row[1]))
 
-# Calculate the required values
+# Figure out all the data needed
+
+# Total months is the length (or quantity) of items in the list "dates"
 total_months = len(dates)
+
+# Total profit is the sum of the profits list
 total_profit = sum(profits)
+
+# Figure changes as a list that is made using a for loop that compares the differences in the second row with the first and records it
 changes = [profits[i+1]-profits[i] for i in range(len(profits)-1)]
+
+# Then we just need to figure out the average of changes, so we just add changes and divide it by the quantity of datapoints (len)
 average_change = sum(changes)/len(changes)
+
+# Highest number is the max in the changes list
 greatest_increase = max(changes)
+
+# Lowest number is the min in the changes list
 greatest_decrease = min(changes)
+
+# This was really tricky, basically saying the greatest increase date is the dates list where the called index is the index of the greatest increase in the changes list but we have to add one because the changes list is one row shorter since the range was len(profits)-1. we reduced the range of changes by 1 because we technically skipped the first row since we only calculate the changes between two numbers so inevitably the list is shorter by 1.
 greatest_increase_date = dates[changes.index(greatest_increase)+1]
+
+# Same thing but for decrease
 greatest_decrease_date = dates[changes.index(greatest_decrease)+1]
 
-# Print the analysis to the terminal
+# Print the analysis to the terminal, make it match the assignment layout
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
@@ -29,12 +51,22 @@ print(f"Average Change: ${average_change:.2f}")
 print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
 
+# Print the header for fun just as proof that the header was successfully stored
+print(f"The header rows were: {header}")
+
 # Export a text file with the results
-with open('financial_analysis.txt', 'w') as outfile:
+with open('PyBank_Summary.txt', 'w') as outfile:
     outfile.write("Financial Analysis\n")
     outfile.write("----------------------------\n")
     outfile.write(f"Total Months: {total_months}\n")
     outfile.write(f"Total: ${total_profit}\n")
+    
+    # Need to use .2f to format the decimals in Average Change
     outfile.write(f"Average Change: ${average_change:.2f}\n")
+    
+    # Need to make sure to use $ to display the currency format
     outfile.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n")
     outfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
+    
+    # Write the header for fun just as proof that the header was successfully stored
+    outfile.write(f"The header rows were: {header}\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,37 +1,40 @@
 import csv
 
-# Set file path for the data
+# Source the file from election_data.csv
 file_path = "Resources/election_data.csv"
 
-# Initialize variables to store data
+# To start, set total votes to 0, candidates as a dictionary, winner votes to 0 and winner name to blank
 total_votes = 0
 candidates = {}
 winner_votes = 0
 winner_name = ""
 
 # Open the CSV file and read the data
-with open(file_path, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(file_path, newline="") as election_data_csv:
+    read_election_data_csv = csv.reader(election_data_csv, delimiter=",")
     
-    # Skip header row
-    next(csvreader)
+    # Stores the header row
+    header = next(election_data_csv) 
     
-    # Iterate through each row of data
-    for row in csvreader:
-        # Count the total number of votes cast
+    # Loop the data in the csv
+    for row in read_election_data_csv:
+        
+        # Keep a tally of the total votes, I discovered that += will loop and increment so I'll use that instead of total_votes + 1
         total_votes += 1
         
-        # Keep track of the vote count for each candidate
+        # Keep the vote count using a similar methodology
         candidate_name = row[2]
         if candidate_name not in candidates:
             candidates[candidate_name] = 0
         candidates[candidate_name] += 1
 
-# Print the results to the terminal
+# Print results to the terminal
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
+
+# Loop the candidates and display their data
 for candidate in candidates:
     # Calculate the percentage of votes each candidate won
     vote_percentage = (candidates[candidate] / total_votes) * 100
@@ -39,24 +42,34 @@ for candidate in candidates:
     # Print the candidate's name, percentage of votes, and vote count
     print(f"{candidate}: {vote_percentage:.3f}% ({candidates[candidate]})")
     
-    # Check if this candidate has more votes than the current winner
+    # Return the winner based on the loop data
     if candidates[candidate] > winner_votes:
         winner_votes = candidates[candidate]
         winner_name = candidate
 
 print("-------------------------")
+
+# Print the winner name
 print(f"Winner: {winner_name}")
 print("-------------------------")
 
-# Export the results to a text file
-with open("election_results.txt", "w") as outfile:
+# Print the header for fun just as proof that the header was successfully stored
+print(f"The header rows were: {header}")
+
+# Make a .txt with the data formatted to the way the module assignment showed
+with open("PyPoll_Summary.txt", "w") as outfile:
     outfile.write("Election Results\n")
     outfile.write("-------------------------\n")
     outfile.write(f"Total Votes: {total_votes}\n")
     outfile.write("-------------------------\n")
+    
+    # Loop all the candidates and figure out what the vote percentage is using total_votes
     for candidate in candidates:
         vote_percentage = (candidates[candidate] / total_votes) * 100
         outfile.write(f"{candidate}: {vote_percentage:.3f}% ({candidates[candidate]})\n")
     outfile.write("-------------------------\n")
     outfile.write(f"Winner: {winner_name}\n")
     outfile.write("-------------------------\n")
+    
+    # Write the header for fun just as proof that the header was successfully stored
+    outfile.write(f"The header rows were: {header}\n")
